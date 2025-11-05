c8447148631861c4554f4aa97e2fb5bfb70fc0b9
studentFadeeva <hohoho_lena@mail.ru>
2025-11-05 21:13:26 +0500

    Изменения от 03.11.2025

---
+++ class_work_1/work_3.py
@@ -0,0 +1,23 @@
+# numbers = int (input("ввести число: "))
+# counter = 1
+# while counter != numbers+1:
+#     print(counter)
+#     counter = counter + 1
+
+# num1 = 1
+# num2 = int (input("ввести число: "))
+# result = num1 + num2
+# counter = 1
+# while counter != result+1:
+#     print(counter)
+#     counter = counter + 1
+
+# numbers = int (input("ввести число: "))
+# for x in range (numbers+1):
+#     print(x)
+
+num1 = 1
+num2 = int (input("ввести число: "))
+result = ("num1 + num2")
+for x in range (result):
+    print(x)
