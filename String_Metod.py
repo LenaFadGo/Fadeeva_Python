5948b4c2908f8c3ea1513bcc3c1582f2ede7c4b4
studentFadeeva <hohoho_lena@mail.ru>
2025-11-10 21:12:55 +0500

    Изменения от 10.11.2025

---
+++ String_Metod.py
@@ -0,0 +1,20 @@
+# Задание 1
+
+# num_1 = int(input("Введите первое число: "))
+# num_2 = int(input("Введите второе число: "))
+# for number in range(num_1, num_2 + 1):
+#     if number % 7 == 0:
+#         print(number)
+
+# Задание 2
+# start = int(input("Число начала диапазона: "))
+# stop = int(input("Число конца диапазона: "))
+# for x in range(start, stop+1):
+#     print(x)
+
+# start = int(input("Число начала диапазона: "))
+# stop = int(input("Число конца диапазона: "))
+# for x in range(stop,start,-1):
+#     print(x)
+
+
