459bb8dce09b5a0d972139f7a15791d97d51dc02
RERIK <glebkrot200800@gmail.com>
2025-11-19 21:18:11 +0500

    Изменения от 10.11.2025

---
+++ Practic/19_11_2025.py
@@ -0,0 +1,28 @@
+# Задание 1
+
+# text = input("Ввести текст: ")
+# x = text[0].upper()
+# f = False
+# for b in range(1, len(text)):
+#     if f:
+#         x = x + text[b].upper()
+#         f = False
+#     else:
+#         x = x + text[b]
+#     if text[b] == "":
+#         f = True
+# print(x)
+
+#  text = input("Вводим текст: ")
+# def capitalize_sentences(text):
+#     sentences = text.split('. ')
+#     capitalized = [sentence.capitalize() for sentence in sentences]
+#     return '. '.join(capitalized)
+# result = capitalize_sentences(text)
+# print(result)
+
+
+
+
+
+
