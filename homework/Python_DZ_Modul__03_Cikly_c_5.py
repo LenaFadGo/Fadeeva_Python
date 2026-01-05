bb8edf048e4c8f07f0ea51b4524c810357e4d6cb
RERIK <glebkrot200800@gmail.com>
2026-01-05 21:12:25 +0500

    Занятие 08.12.2025

--- homework/Python_DZ_Modul__03_Cikly_c_5.py
+++ homework/Python_DZ_Modul__03_Cikly_c_5.py
@@ -4,20 +4,25 @@ print("б")
 print("в")
 print("г")
 print("д")
+print("е")
+print("ж")
+print("з")
+print("и")
+print("к")
 
 choice = input("Ваш выбор: ")
 if choice == "а":
-    n = 10
+    n = 6
     for i in reversed(range(1,n+1)):
         print(' ' * (n - i), '*' * i)
 
 if choice == "б":
-    n = 10
+    n = 6
     for i in range(n):
         print('*' * (i + 1) + ((n - i) * ' '))
 
 if choice == "в":
-    n = 10
+    n = 6
     k = 2 * n - 2
     for i in range(n, -1, -1):
         for j in range(k, 0, -1):
@@ -28,12 +33,33 @@ if choice == "в":
         print("")
 
 if choice == "г":
-    n = 10
+    n = 5
     for i in range (1,n+1):
         print(" " * n, end='')
         print('* ' * (i))
         n -= 1
 
+if choice == "д":
+    n = 5
+    for i in range(n):
+        spaces = ' ' * i
+        stars = '*' * (2 * (n - i) - 1)
+        print(spaces + stars)
+    for i in range(1, n):
+        spaces = ' ' * (n - i - 1)
+        stars = '*' * (2 * i + 1)
+        print(spaces + stars)
+
+if choice == "е":
+    n=6# ширина в звездах
+    for i in range (n):
+        stars = "*" * (i+1)
+        spaces = ' ' * (2 * (n- i - 1))
+        print(stars + spaces + stars)
+    for i in range (n-2,-1,-1):
+        stars = "*" * (i+1)
+        spaces = ' ' * (2 * (n-i-1))
+        print(stars + spaces + stars)
 
 
 
