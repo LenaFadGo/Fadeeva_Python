a603bac6e42a514163bc988c1d59d0d3ff4053c7
student_fun_15_01_2026 <hohoho_lena@mail.ru>
2026-01-15 21:18:04 +0500

    Работа в классе 15.01.2026

---
+++ homework/functions/Module 5_Functions_Topic_Functions_Part_1.py
@@ -0,0 +1,35 @@
+# Задание № 1
+# def print_quote ():
+#     quote = (
+#         "“Don't compare yourself with anyone in this world…\n"
+#         " if you do so, you are insulting yourself.”\n"
+#         "                                        Bill Gates")
+#     print(quote)
+# print_quote ()
+
+# Задание № 2
+# def print_input_numbers ():
+#     start = int(1)
+#     finish = int(12)
+#     for num in range(start, finish):
+#         if num % 2 == 0:
+#             print(num)
+# print_input_numbers()
+
+# Задание № 3
+# def draw_square (side, sign, filled_square):
+#     if side <= 0:
+#         print()
+#     for side in range (side):
+#         if filled_square ():
+#             print(side * sign)
+#         return
+
+
+
+
+
+
+
+
+
